7bab4f02e952141bb41e69323ccc7101c39b4997
shiblymohammed <shibilymohammed75@gmail.com>
2026-02-18 01:29:37 +0530

    uses another library///cacaca

--- src/app/ar/page.tsx
+++ src/app/ar/page.tsx
@@ -1,9 +1,33 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { XR, createXRStore } from "@react-three/xr";
+import { XR, createXRStore, useXRHitTest } from "@react-three/xr";
+import { useRef } from "react";
+import * as THREE from "three";
 
 const store = createXRStore();
+
+function Reticle() {
+  const ref = useRef<THREE.Mesh>(null);
+
+  useXRHitTest((results: XRHitTestResult[], getWorldMatrix: any) => {
+    if (results.length > 0 && ref.current) {
+      const hitMatrix = getWorldMatrix(results[0]);
+      ref.current.visible = true;
+      ref.current.matrix.copy(hitMatrix);
+      ref.current.matrixAutoUpdate = false;
+    } else if (ref.current) {
+      ref.current.visible = false;
+    }
+  }, "viewer");
+
+  return (
+    <mesh ref={ref} visible={false}>
+      <ringGeometry args={[0.1, 0.15, 32]} />
+      <meshBasicMaterial color="#00ff00" side={THREE.DoubleSide} />
+    </mesh>
+  );
+}
 
 export default function ARPage() {
   return (
@@ -30,10 +54,15 @@ export default function ARPage() {
       <Canvas>
         <XR store={store}>
           <ambientLight intensity={1} />
+          
+          {/* Test cube - always visible */}
           <mesh position={[0, 0, -1]}>
             <boxGeometry args={[0.2, 0.2, 0.2]} />
             <meshStandardMaterial color="red" />
           </mesh>
+
+          {/* Reticle - shows on surfaces */}
+          <Reticle />
         </XR>
       </Canvas>
     </div>
